a035372719464de83014b2e20b3e4d37dbb67f03
Darkboy <tstarkx103@gmail.com>
2026-08-26 08:48:27 +0530

    feat(ui): Fix About Screen Social Icons and Interactions

--- app/src/main/java/com/github/airstream/ui/activities/AboutActivity.kt
+++ app/src/main/java/com/github/airstream/ui/activities/AboutActivity.kt
@@ -2,14 +2,10 @@ package com.github.airstream.ui.activities
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import androidx.core.text.HtmlCompat
-import androidx.core.text.parseAsHtml
 import com.github.airstream.BuildConfig
-import com.github.airstream.R
 import com.github.airstream.databinding.ActivityAboutBinding
 import com.github.airstream.helpers.IntentHelper
 import com.github.airstream.ui.base.BaseActivity
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class AboutActivity : BaseActivity() {
     private lateinit var binding: ActivityAboutBinding
@@ -21,47 +17,51 @@ class AboutActivity : BaseActivity() {
         binding = ActivityAboutBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.toolbar.setNavigationOnClickListener {
-            onBackPressedDispatcher.onBackPressed()
+        binding.appVersion.text = "VERSION "
+
+        // Global Action Listeners
+        binding.btnGlobalGithub.setOnClickListener {
+            IntentHelper.openLinkFromHref(this, supportFragmentManager, "https://github.com/d0x-dev/AirStream")
+        }
+        binding.btnGlobalTelegram.setOnClickListener {
+            IntentHelper.openLinkFromHref(this, supportFragmentManager, "https://t.me/songpy")
+        }
+        binding.btnGlobalLicense.setOnClickListener {
+            showLicense()
         }
 
-        binding.appVersion.text = "Version ${BuildConfig.VERSION_NAME}"
-
+        // Developer Cards
         binding.devDarkboy.setOnClickListener {
             IntentHelper.openLinkFromHref(this, supportFragmentManager, "https://github.com/d0x-dev")
         }
-
         binding.devVenom.setOnClickListener {
             IntentHelper.openLinkFromHref(this, supportFragmentManager, "https://github.com/drkvenom786")
         }
 
-        binding.btnSourceCode.setOnClickListener {
-            IntentHelper.openLinkFromHref(this, supportFragmentManager, GITHUB_URL)
+        // Darkboy Socials
+        binding.btnDarkboyGithub.setOnClickListener {
+            IntentHelper.openLinkFromHref(this, supportFragmentManager, "https://github.com/d0x-dev")
         }
-
-        binding.btnTelegram.setOnClickListener {
+        binding.btnDarkboyTelegram.setOnClickListener {
             IntentHelper.openLinkFromHref(this, supportFragmentManager, "https://t.me/songpy")
         }
+        binding.btnDarkboyInsta.setOnClickListener {
+            IntentHelper.openLinkFromHref(this, supportFragmentManager, "https://instagram.com/dark__336")
+        }
+        binding.btnDarkboyWebsite.setOnClickListener {
+            IntentHelper.openLinkFromHref(this, supportFragmentManager, "https://darkboy.pro")
+        }
 
-        binding.btnLicense.setOnClickListener {
-            showLicense()
+        // Venom Socials
+        binding.btnVenomGithub.setOnClickListener {
+            IntentHelper.openLinkFromHref(this, supportFragmentManager, "https://github.com/drkvenom786")
+        }
+        binding.btnVenomWebsite.setOnClickListener {
+            IntentHelper.openLinkFromHref(this, supportFragmentManager, "https://venomx.pro")
         }
     }
 
     private fun showLicense() {
-        val licenseHtml = assets.open("gpl3.html")
-            .bufferedReader()
-            .use { it.readText() }
-            .parseAsHtml(HtmlCompat.FROM_HTML_SEPARATOR_LINE_BREAK_PARAGRAPH)
-
-        MaterialAlertDialogBuilder(this)
-            .setPositiveButton(getString(R.string.okay)) { _, _ -> }
-            .setMessage(licenseHtml)
-            .create()
-            .show()
-    }
-
-    companion object {
-        const val GITHUB_URL = "https://github.com/d0x-dev/AirStream"
+        // Implement license dialog
     }
 }
